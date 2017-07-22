04a57f66a463ada99b483a5962b949cf67c12f2f
Stephen Cooper <Stephen.Cooper@cascadehr.co.uk>
2017-07-22 22:08:30 +0100

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+// This is free and unencumbered software released into the public domain.
+// See LICENSE for details
+
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
+const {autoUpdater} = require("electron-updater");
+let win;
+
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/index.html`);
+  return win;
+}
+
+autoUpdater.on('update-downloaded', (info) => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 5 seconds.
+  // You could call autoUpdater.quitAndInstall(); immediately
+  setTimeout(function() {
+    autoUpdater.quitAndInstall();  
+  }, 5000)
+})
+
+app.on('ready', function()  {
+    createDefaultWindow();
+  autoUpdater.checkForUpdates();
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
